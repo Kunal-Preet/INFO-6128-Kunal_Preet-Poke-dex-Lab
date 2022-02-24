1085fa5fe697a68e1bbeee1ce152a926f07e2f4c
Kunal Preet <kunalpreet@Kunals-MacBook-Air.local>
2022-02-24 19:27:03 +0530

    commit3

---
+++ src/app.js
@@ -0,0 +1,25 @@
+let url = 'https://pokeapi.co/api/v2/pokemon?limit=100';
+let nextPokenumber = 1; // use to keep track of the Pokémon numbers
+
+const get = async () => {
+  // do the API call and get JSON response
+  const response = await fetch(url);
+  const json = await response.json();
+
+  const newPokemon = json.results.map(e => e.name);
+
+  const list = document.querySelector('#pokemon-list');
+  newPokemon.forEach(name => {
+    list.appendChild(ons.createElement(`
+      <ons-list-item modifier="chevron" tappable expandable >
+        ${nextPokenumber} ${name}
+        <div class="expandable-content">
+          <ons-button onclick="savePokemon(${nextPokenumber}, this)">Save</ons-button>
+        </div>
+      </ons-list-item>
+    `));
+    nextPokenumber++;
+  });
+
+  url = json.next;
+};
